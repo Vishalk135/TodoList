6d97d743dade875a918fbc7e5c8cf227c55e1ca4
vishal <22lco04@aitdgoa.edu.in>
2025-06-14 15:07:36 +0530

    update index.js

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,33 @@
-import app from './app';
-import connectDB from './config/db';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import taskRoutes from './routes/taskRoute';
 
 dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 4000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/tasks', taskRoutes);
+app.get('/', (_req, res) => {
+  res.send('To-Do List API is running!');
 });
+
+// Connect to MongoDB and start the server
+mongoose.connect(process.env.MONGO_URI!)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
