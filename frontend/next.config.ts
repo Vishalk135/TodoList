d1ad02edeb6361b92cba3ff119eaf4d9e19eb7fc
vishal <22lco04@aitdgoa.edu.in>
2025-06-15 03:47:17 +0530

    frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
+  reactStrictMode: true,
+  output: 'export', // Important for Netlify static deployment
 };
 
 export default nextConfig;
