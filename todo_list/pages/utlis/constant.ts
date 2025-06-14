8ee82a17e45c9fbe396f0e916a9f6b510ed7e624
vishal <22lco04@aitdgoa.edu.in>
2025-06-14 22:50:33 +0530

    constant.js

--- todo_list/pages/utlis/constant.ts
+++ todo_list/pages/utlis/constant.ts
@@ -1,2 +1,2 @@
-export const baseURL: string = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000/tasks';
+export const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000/tasks';
 //export const baseURL = https://todo-list-zn4l.vercel.app/tasks;
