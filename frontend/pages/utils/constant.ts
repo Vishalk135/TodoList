1b4585de71ac87bf799fa66b225ba4661530658c
vishal <22lco04@aitdgoa.edu.in>
2025-06-15 02:23:17 +0530

    frontend

--- frontend/pages/utils/constant.ts
+++ frontend/pages/utils/constant.ts
@@ -1,3 +1,3 @@
-export const baseURL = 'http://localhost:4000/tasks';
+export const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000/tasks';
 
 //export const baseURL = https://todo-list-zn4l.vercel.app/tasks; //vercel backend url
