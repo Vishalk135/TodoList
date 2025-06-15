28e9fbe2ef8b8d82eebff2491f5b7e70d441e44b
Vishalk135 <22lco04@aitdgoa.edu.in>
2025-06-15 19:36:28 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors({
-  origin: ["https://todo-list-full-stack-project.netlify.app"]
+  origin: "https://todo-list-full-stack-project.netlify.app"
 }));
 app.use(bodyParser.json());
 
